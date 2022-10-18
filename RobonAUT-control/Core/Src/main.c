105d6414279f6e790d547c4b5e52d977a2344a44
@szalay2318 <szalay2318@gmail.com>
2022-10-18 16:34:41 +0200

    zsombi commit

--- RobonAUT-control/Core/Src/main.c
+++ RobonAUT-control/Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-int kakaarc = 1;
+int kakaarc = 2;
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
