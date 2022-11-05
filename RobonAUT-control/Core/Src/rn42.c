23acf288ef50bba00f8bcb8f6399baf12bfff7ae
@szalay2318 <szalay2318@gmail.com>
2022-11-05 14:57:03 +0100

    Félig működő RN42

--- RobonAUT-control/Core/Src/rn42.c
+++ RobonAUT-control/Core/Src/rn42.c
@@ -18,7 +18,7 @@ uint8_t Rn42_Init(UART_HandleTypeDef *uart_rn42, UART_HandleTypeDef *uart_pc)
 	memset(rx_buf,0,30);
 
 	TEL_7(0);
-	TEL_4(1);
+	TEL_4(0);
 	HAL_Delay(501);
 	//command mód
 	sprintf(tx_buf,"$$$");
@@ -29,6 +29,38 @@ uint8_t Rn42_Init(UART_HandleTypeDef *uart_rn42, UART_HandleTypeDef *uart_pc)
 	memset(tx_buf,0,30);
 	memset(rx_buf,0,30);
 
+	sprintf(tx_buf,"SF,1\r\n");
+	HAL_UART_Transmit(uart_rn42, tx_buf, strlen(tx_buf), 100); //kikuljuk a comandot
+	HAL_UART_Receive(uart_rn42,rx_buf, 5, 1000); //fogadjuk a választ (CMD\n\R)
+	HAL_UART_Transmit(uart_pc, rx_buf, strlen(rx_buf), 100); //ezt kiiratjuk magunknak
+	HAL_Delay(2000);
+	memset(tx_buf,0,30);
+	memset(rx_buf,0,30);
+
+	sprintf(tx_buf,"R,1\r\n");
+	HAL_UART_Transmit(uart_rn42, tx_buf, strlen(tx_buf), 100); //kikuljuk a comandot
+	HAL_UART_Receive(uart_rn42,rx_buf, 9, 2000); //fogadjuk a választ (CMD\n\R)
+	HAL_UART_Transmit(uart_pc, rx_buf, strlen(rx_buf), 100); //ezt kiiratjuk magunknak
+	HAL_Delay(2000);
+	memset(tx_buf,0,30);
+	memset(rx_buf,0,30);
+
+	sprintf(tx_buf,"$$$");
+	HAL_UART_Transmit(uart_rn42, tx_buf, strlen(tx_buf), 100); //kikuljuk a comandot
+	HAL_UART_Receive(uart_rn42,rx_buf, 5, 200); //fogadjuk a választ (CMD\n\R)
+	HAL_UART_Transmit(uart_pc, rx_buf, strlen(rx_buf), 100); //ezt kiiratjuk magunknak
+	HAL_Delay(100);
+	memset(tx_buf,0,30);
+	memset(rx_buf,0,30);
+
+	sprintf(tx_buf,"SM,0\r\n");
+	HAL_UART_Transmit(uart_rn42, tx_buf, strlen(tx_buf), 100); //kikuljuk a comandot
+	HAL_UART_Receive(uart_rn42,rx_buf, 5, 200); //fogadjuk a választ (CMD\n\R)
+	HAL_UART_Transmit(uart_pc, rx_buf, strlen(rx_buf), 100); //ezt kiiratjuk magunknak
+	HAL_Delay(100);
+	memset(tx_buf,0,30);
+	memset(rx_buf,0,30);
+/**/
 	//connect a megfelelő fizikai cimre
 	sprintf(tx_buf,"C,70C94ED33AC8\r");
 	HAL_UART_Transmit(uart_rn42, tx_buf, strlen(tx_buf), 300);
@@ -38,7 +70,7 @@ uint8_t Rn42_Init(UART_HandleTypeDef *uart_rn42, UART_HandleTypeDef *uart_pc)
 	memset(tx_buf,0,30);
 	memset(rx_buf,0,30);
 
-/**/	//connection status olvasás
+	//connection status olvasás
 	sprintf(tx_buf,"GK\r");
 	HAL_UART_Transmit(uart_rn42, tx_buf, strlen(tx_buf), 100);
 	HAL_UART_Receive(uart_rn42,rx_buf, 8, 200);
